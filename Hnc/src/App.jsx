b2abda437c7eb632f5187038c4485a97b08bd990
Aman Sharma <amansharma.as536@gmail.com>
2025-01-25 14:37:12 +0530

    faqs done

---
+++ Hnc/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Faq from './components/faq'
+
+function App() {
+
+  return (
+    <>
+      <Faq/>
+    </>
+  )
+}
+
+export default App
---
+++ Hnc/tailwind.config.js
@@ -0,0 +1,70 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    darkMode: ["class"],
+    content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+  	extend: {
+  		colors: {
+        clr1: '#EA4740',
+  		clr2: '#419A94',
+  		clr3: '#B5DCBF',
+  		clr4: '#FFCDDE',
+  		clr5: '#F96A96',
+  			main: 'var(--main)',
+  			overlay: 'var(--overlay)',
+  			bg: 'var(--bg)',
+  			bw: 'var(--bw)',
+  			blank: 'var(--blank)',
+  			text: 'var(--text)',
+  			mtext: 'var(--mtext)',
+  			border: 'var(--border)',
+  			ring: 'var(--ring)',
+  			ringOffset: 'var(--ring-offset)',
+  			secondaryBlack: '#212121'
+  		},
+  		borderRadius: {
+  			base: '5px'
+  		},
+  		boxShadow: {
+  			shadow: 'var(--shadow)'
+  		},
+  		translate: {
+  			boxShadowX: '4px',
+  			boxShadowY: '4px',
+  			reverseBoxShadowX: '-4px',
+  			reverseBoxShadowY: '-4px'
+  		},
+  		fontWeight: {
+  			base: '500',
+  			heading: '700'
+  		},
+  		keyframes: {
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			}
+  		},
+  		animation: {
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out'
+  		}
+  	}
+  },
+  plugins: [require("tailwindcss-animate")],
+}
+
