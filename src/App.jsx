c97625f7b3600a9189147e4a2e43fa8a4584b4fe
Divyansh <divyanshraj0408@gmail.com>
2025-01-27 00:53:39 +0530

    about us complete

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <div>
       <Navbar />
       <Herosection />
-      {/* <About /> */}
+      <About />
       {/* <Partners />
       <Faq /> */}
     </div>
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,8 +2,22 @@ import "./About.css";
 const About = () => {
   return (
     <>
-      <div className="AboutUs">
-        <h1>About us</h1>
+      <div className="AboutUs container">
+        <div className="aboutUS-heading">
+          <img src="/pics/aboutus.svg" alt="" />
+        </div>
+        <div className="aboutus-content">
+          <img src="/pics/HNC1.JPG" alt="" />
+          <p>
+            Get ready for an exciting dive into innovation and collaboration as
+            we proudly unveil the grandest event of our GDGC ADGIPS Hack&Chill
+            2.0! It's time to announce the most anticipated Hackathon of the
+            year, coming to you this March with promises of unparalleled thrills
+            and a celebration of technology like never before. Hack&Chill 2.0
+            introduces a spectrum of captivating themes, offering substantial
+            prize money for outstanding projects in each theme.
+          </p>
+        </div>
       </div>
     </>
   );
