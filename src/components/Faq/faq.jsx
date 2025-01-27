77547ca6ecb12a7c8ed5fb5d01d35d900c0da8eb
Divyansh <divyanshraj0408@gmail.com>
2025-01-27 17:19:56 +0530

    faq complete

--- src/components/Faq/faq.jsx
+++ src/components/Faq/faq.jsx
@@ -1,36 +1,49 @@
 import React from "react";
-import "./faq.css";
 import { ChevronDown, ChevronUp } from "lucide-react";
 import Accordion from "../accordion";
+import "./faq.css";
 
-const faq = () => {
+const FAQ = () => {
   return (
-    <div id="faq" className="bg-bgc">
-      <div className="spandiv relative bg-bgc rounded-xl border-black border-2">
-        <span className="text-clr2">FREQUENTLY</span>
-        <span className="text-black">ASKED</span>
-        <span className="text-clr1">QUESTIONS</span>
+    <div id="faq" className="w-full">
+      <div className="w-full flex justify-center items-center py-8">
+        <img src="/pics/faq.svg" alt="FAQ" className="w-[15%] h-auto" />
       </div>
-      <div className="list flex flex-wrap min-w-full p-3  justify-around">
-        <Accordion
-          heading="What is a hackathon"
-          des=" A hackathon is blah blah"
-        />
-        <Accordion
-          heading="What is a hackathon"
-          des=" A hackathon is blah blah"
-        />
-        <Accordion
-          heading="What is a hackathon"
-          des=" A hackathon is blah blah"
-        />
-        <Accordion
-          heading="What is a hackathon"
-          des=" A hackathon is blah blah"
-        />
+
+      <div className="w-full  mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-8">
+          <div className="w-[100%] flex justify-center">
+            <Accordion
+              heading="What is a hackathon"
+              des="A hackathon is blah blah"
+              className="p-50px bg-red-400 shadow-lg"
+            />
+          </div>
+          <div className="w-[100%] flex justify-center">
+            <Accordion
+              heading="What is a hackathon"
+              des="A hackathon is blah blah"
+              className="w-full bg-red-400 shadow-lg"
+            />
+          </div>
+          <div className="w-[100%] flex justify-center">
+            <Accordion
+              heading="What is a hackathon"
+              des="A hackathon is blah blah"
+              className="w-full bg-red-400 shadow-lg"
+            />
+          </div>
+          <div className="w-[100%] flex justify-center">
+            <Accordion
+              heading="What is a hackathon"
+              des="A hackathon is blah blah"
+              className="w-full bg-red-400 shadow-lg"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
 };
 
-export default faq;
+export default FAQ;
